15f239ae0cc3e48ece8e7dfcc4270d65571576bb
Tejashvi Kumawat <148739094+tejashvi-kumawat@users.noreply.github.com>
2025-08-26 00:27:34 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import styles from './App.module.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Login from './Pages/Login';
-import Home from './Pages/Home';
+import Login from './Pages/Login/Login.tsx';
+import Home from './Pages/Home/Home.tsx';
 import './styles/styles.css';
 
 const App: React.FC = () => (
