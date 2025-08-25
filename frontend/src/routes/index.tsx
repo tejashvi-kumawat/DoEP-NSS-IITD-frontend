fed0e71edfd5a0d3b354718f396f837e7a8a38c7
Tejashvi Kumawat <148739094+tejashvi-kumawat@users.noreply.github.com>
2025-08-26 00:33:37 +0530

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,14 @@
-// src/routes/index.ts
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const AppRoutes: React.FC = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 );
-
 export default AppRoutes;
