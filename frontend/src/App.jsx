44d0c0ed6de1bc4f9404e02e8e74fd009ccf2632
Tejashvi Kumawat <148739094+tejashvi-kumawat@users.noreply.github.com>
2025-09-24 18:51:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Registration';
-import Register from './pages/VolunteerRegister';
+import VolunteerRegister from './pages/VolunteerRegister';
 
 const App = () => {
   return (
