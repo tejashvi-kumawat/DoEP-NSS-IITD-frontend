07206fdf7b6bd6f223cb7e8f8eb19217368b6fa1
divyanshnagpal776-ops <divyanshnagpal776@gmail.com>
2025-10-02 01:28:36 +0530

    Update App.jsx

--- projectFrontend/src/App.jsx
+++ projectFrontend/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectHome from './pages/ProjectHome'
 import ProjectNavbar from './components/Navbar';
+import GalleryPage from './pages/GalleryPage';
+import ResourcesPage from "./pages/ResourcesPage.jsx";
+import TeamPage from "./pages/TeamPage.jsx";
 
 const App = () => {
   return (
@@ -11,6 +14,10 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<ProjectHome />} />
+            <Route path="/gallery" element={<GalleryPage />} /> {/* add GalleryPage route */}\
+            <Route path="/team" element={<TeamPage />} /> {/* add GalleryPage route */}
+            <Route path="/resources" element={<ResourcesPage />} /> {/* add GalleryPage route */}
+
         </Routes>
       </div>
     </Router>
